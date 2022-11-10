dd78b72745bf50fefbd470b2e1f8fa29e55cdb53
ckcomin <com20009@byui.edu>
2022-11-09 17:03:54 -0700

    layout

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -15,23 +15,35 @@ fetch(requestURL)
 function displayCompanies(company) {  // Create elements to add to the document
     let card = document.createElement('section');
     let image = document.createElement('img');
-    let h2 = document.createElement('h2');    
+    let p3 = document.createElement('p');    
     let p = document.createElement('p');   
     let p2 = document.createElement('p');
     let a = document.createElement('a');
     // Change the textContent property of the h2 element to contain the company's full name
     image.setAttribute('src', company.imageurl);
     image.setAttribute('alt', company.name);
+    p3.textContent = company.name;
     p.textContent = company.address;
     p2.textContent = company.phone;
     a.textContent = company.website;
 
     // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
+    card.appendChild(image);
+    card.appendChild(p3);
     card.appendChild(p);
     card.appendChild(p2);
-    card.appendChild(image);
+    card.appendChild(a);
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card); 
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("sList").onclick = () => {
+    document.getElementById("lgDemo").classList.add("list");
+  };
+  document.getElementById("sGrid").onclick = () => {
+    document.getElementById("lgDemo").classList.remove("list");
+    
+  };
+});
 
