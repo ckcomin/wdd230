4bc173dadfce22f532d82e67a619d8a0cbf6182e
ckcomin <com20009@byui.edu>
2022-11-18 14:47:46 -0700

    weather api

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,15 +1,19 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=05672,us&appid=1a3eac6be9133ba2f1b3961097ba91b2&units=imperial';
+// const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=05672,us&appid=1a3eac6be9133ba2f1b3961097ba91b2&units=imperial';
 
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.querySelector('#current-temp').textContent = jsObject.main.temp;
-    document.querySelector('#speed').textContent = jsObject.wind.speed;
-    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    const desc = jsObject.weather[0].description;
-    document.querySelector('#weathericon').setAttribute('src', iconsrc);
-    document.querySelector('#weathericon').setAttribute('alt', desc);
-    document.querySelector('figcaption').textContent = desc;
-});
+// fetch(apiURL)
+//   .then((response) => response.json())
+//   .then((jsObject) => {
+//     console.log(jsObject);
+//     document.querySelector('#current-temp').textContent = jsObject.main.temp;
+//     document.querySelector('#speed').textContent = jsObject.wind.speed;
+//     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+//     const desc = jsObject.weather[0].description;
+//     document.querySelector('#weathericon').setAttribute('src', iconsrc);
+//     document.querySelector('#weathericon').setAttribute('alt', desc);
+//     document.querySelector('#weatherdesc').textContent = desc;
+//   }
+// );
+
+
+
 
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,9 +1,15 @@
-function setwindchill(temp, windspeed){
+function setwindchill(obj){
     let tempobj = document.querySelector('#current-temp');
     let windspeedobj = document.querySelector('#speed');
     let windchillobj = document.querySelector('#chill');
+    let weathericon = document.querySelector('#weathericon');
+    let weatherdesc = document.querySelector('#weatherdesc');
+    let iconsrc= `https://openweathermap.org/img/w/${obj.weather[0].icon}.png`;
 
     let windchillmsg = "N/A";
+
+    let temp = obj.main.temp;
+    let windspeed = obj.wind.speed
 
     if (temp <= 50 && windspeed > 3){
         let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
@@ -13,10 +19,17 @@ function setwindchill(temp, windspeed){
     tempobj.textContent = temp;
     windspeedobj.textContent = windspeed;
     windchillobj.textContent = windchillmsg;
+    weathericon.setAttribute('src', iconsrc);
+    weathericon.setAttribute('alt', weatherdesc);
+    const desc = obj.weather[0].description;
+    weatherdesc.textContent = desc
 
 }
 
-let tem = document.querySelector('#current-temp');
-let speed = document.querySelector('#speed');
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=05672,us&appid=1a3eac6be9133ba2f1b3961097ba91b2&units=imperial';
 
-setwindchill(tem, speed)
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    setwindchill(jsObject);
+  });
