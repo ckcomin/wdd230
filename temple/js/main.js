9d70b72034a2cbea091fbf246237251e2de951fe
ckcomin <com20009@byui.edu>
2022-11-29 22:46:26 -0700

    start of the final

---
+++ temple/js/main.js
@@ -0,0 +1,27 @@
+//hamburger
+
+function toggleMenu() {
+    document.getElementById("priNav").classList.toggle("open");
+    document.getElementById("hamBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamBtn")
+x.onclick = toggleMenu;
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
