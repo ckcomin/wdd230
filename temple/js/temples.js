a47aa032cbfd1f5de6579e926ad79b4a233072ec
ckcomin <com20009@byui.edu>
2022-11-30 16:41:30 -0700

    json

--- temple/js/temples.js
+++ temple/js/temples.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://ckcomin.github.io/wdd230/chamber/data.json';
+const requestURL = 'https://ckcomin.github.io/wdd230/temple/json/temples.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -6,31 +6,30 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const companies = jsonObject['companies'];
+    const companies = jsonObject['temples'];
     companies.forEach(displayCompanies);
 });
 
-function displayCompanies(company) {  // Create elements to add to the document
+function displayCompanies(company) {  
+    // Create elements to add to the document
     let card = document.createElement('section');
-    let image = document.createElement('img');
     let h3 = document.createElement('h3');    
     let p = document.createElement('p');   
     let p2 = document.createElement('p');
-    let a = document.createElement('a');
+    let p3 = document.createElement('p');
+
     // Change the textContent property of the h2 element to contain the company's full name
-    image.setAttribute('src', company.imageurl);
-    image.setAttribute('alt', company.name);
     h3.textContent = company.name;
     p.textContent = company.address;
     p2.textContent = company.phone;
-    a.textContent = company.website;
+    p3.textContent = company.services;
 
     // Add/append the section(card) with the h2 element
-    card.appendChild(image);
     card.appendChild(h3);
     card.appendChild(p);
     card.appendChild(p2);
-    card.appendChild(a);
+    card.appendChild(p3);
+
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card); 
 }
