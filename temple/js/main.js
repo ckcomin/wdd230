1b76d954ca95b2de8ac213605189efad5b103240
ckcomin <com20009@byui.edu>
2022-12-10 14:01:19 -0700

    spaces

--- temple/js/main.js
+++ temple/js/main.js
@@ -1,10 +1,6 @@
-//hamburger
-
 function toggleMenu() {
     document.getElementById("priNav").classList.toggle("open");
     document.getElementById("hamBtn").classList.toggle("open");
 }
-
 const x = document.getElementById("hamBtn")
 x.onclick = toggleMenu;
-
